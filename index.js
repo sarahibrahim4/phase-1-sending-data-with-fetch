991b0dc2580ca038c415d38838fa804a1464049f
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-09-18 17:45:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(name,email){
+return fetch('http://localhost:3000/users',{
+  method: 'POST',
+  headers: {'Content-Type': 'application/json',
+Accept :'application/json'},
+body: JSON.stringify({name: name,email: email})
+})
+.then((res)=>res.json())
+.then((data)=>console.log(data))
+.catch((error)=>{alert('error!')
+console.log(error.message)})
+}
+submitData('Sarah','saraibrahim4121@gmail.com')
+submitData('Sara','saraibrahim@gmail.com')
